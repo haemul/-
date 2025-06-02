55976a3164cb42141f2a27ea7b17e0427a24924d
배상진 <91894303+haemul@users.noreply.github.com>
2025-06-02 09:59:31 +0000

    main.cpp 테스트용 임시 수정

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 #include <atomic>
 #include "queue.h"
 
+#include <vector> // 테스트를 위한 임시 추가
+#include <chrono> // 테스트를 위한 임시 추가
+
 using namespace std;
 
 // 초간단 구동 테스트
 // 주의: 아래 정의(Operation, Request)는 예시일 뿐
 // 큐의 Item은 void*이므로 얼마든지 달라질 수 있음
 
-#define REQUEST_PER_CLINET	10000
+#define REQUEST_PER_CLIENT 10000
+#define NUM_CLIENTS 4
 
 atomic<int> sum_key = 0;
 atomic<int> sum_value = 0;
@@ -18,7 +22,6 @@ atomic<int> sum_value = 0;
 typedef enum {
 	GET,
 	SET,
-	GETRANGE
 } Operation;
 
 typedef struct {
@@ -26,70 +29,74 @@ typedef struct {
 	Item item;
 } Request;
 
-void client_func(Queue* queue, Request requests[], int n_request) {
-	Reply reply = { false, 0 };
+void client_func(Queue* queue, Request* requests, int n_request, int client_id) {
+    using namespace chrono;
 
-	// start_time = .....
+    Reply reply;
+    auto start_time = high_resolution_clock::now();
 
-	for (int i = 0; i < n_request; i++) {
-		if (requests[i].op == GET) {
-			reply = dequeue(queue);
-		}
-		else { // SET
-			reply = enqueue(queue, requests[i].item);
-		}
+    for (int i = 0; i < n_request; ++i) {
+        if (requests[i].op == GET) {
+            reply = dequeue(queue);
+        } else {
+            reply = enqueue(queue, requests[i].item);
+        }
 
-		if (reply.success) {
-			// 단순히 리턴받은 키 값을 더함(아무 의미 없음)
-			sum_key += reply.item.key;
-			sum_value += (int)reply.item.value; // void*에서 다시 int로 변환
+        if (reply.success) {
+            sum_key += reply.item.key;
+            sum_value += *(reinterpret_cast<uint8_t*>(reply.item.value));
+        }
+    }
 
-			// 리턴받은 key, value 값 검증
-			// ...생략...
-		}
-		else {
-			// noop
-		}
-	}
+    auto end_time = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end_time - start_time).count();
+    double avg_time = static_cast<double>(duration) / n_request;
 
-	// 진짜로 필요한 건 지연시간을 측정하는 코드
-	//
-	// elapsed_time = finish_time - start_time;
-	// finish_time = ....
-	// average_response_time = elapsed_time / REQUEST_PER_CLIENT;
-	// printf(...average_response_time of client1 = .....);
-	// response_time_tot += finish_time - start_time;
+    cout << "[Client " << client_id << "] Avg response time: " << avg_time << " us" << endl;
 }
 
-int main(void) {
-	srand((unsigned int)time(NULL));
+int main() {
+    srand(static_cast<unsigned int>(time(NULL)));
 
-	// 워크로드 생성(GETRANGE는 패스)
-	Request requests[REQUEST_PER_CLINET];
-	for (int i = 0; i < REQUEST_PER_CLINET / 2; i++) {
-		requests[i].op = SET;
-		requests[i].item.key = i;
-		requests[i].item.value = (void*)(rand() % 1000000);
-	}
-	for (int i = REQUEST_PER_CLINET / 2; i < REQUEST_PER_CLINET; i++) {
-		requests[i].op = GET;
-	}
+    Queue* queue = init();
 
-	Queue* queue = init();
-	//if (queue == NULL) return 0;
+    vector<Request*> all_requests(NUM_CLIENTS);
 
-	// 일단 한 개 뿐인데, 그래도 multi client라고 가정하기
-	thread client = thread(client_func, queue, requests, REQUEST_PER_CLINET);
-	client.join();
+    for (int c = 0; c < NUM_CLIENTS; ++c) {
+        all_requests[c] = new Request[REQUEST_PER_CLIENT];
+        for (int i = 0; i < REQUEST_PER_CLIENT / 2; ++i) {
+            int size = rand() % 1024 + 1;
+            uint8_t* buffer = new uint8_t[size];
+            buffer[0] = rand() % 256;
+            all_requests[c][i].op = SET;
+            all_requests[c][i].item.key = rand() % 10000000;
+            all_requests[c][i].item.value = buffer;
+        }
+        for (int i = REQUEST_PER_CLIENT / 2; i < REQUEST_PER_CLIENT; ++i) {
+            all_requests[c][i].op = GET;
+        }
+    }
 
-	release(queue);
+    vector<thread> clients;
+    auto start = chrono::high_resolution_clock::now();
 
-	// 의미 없는 작업
-	cout << "sum of returned keys = " << sum_key << endl;
-	cout << "sum of returned values = " << sum_value << endl;
+    for (int i = 0; i < NUM_CLIENTS; ++i) {
+        clients.emplace_back(client_func, queue, all_requests[i], REQUEST_PER_CLIENT, i);
+    }
 
-	// 진짜로 필요한 코드
-	// total_average_response_time = total_response_time / n_cleint;
-	// printf("total average response time = ....
-	return 0;
+    for (auto& t : clients) t.join();
+
+    auto finish = chrono::high_resolution_clock::now();
+    auto total_time = chrono::duration_cast<chrono::milliseconds>(finish - start).count();
+
+    cout << "\n[Main] Total time = " << total_time << " ms\n";
+    cout << "[Main] sum of returned keys = " << sum_key << endl;
+    cout << "[Main] sum of returned values = " << sum_value << endl;
+
+    for (int i = 0; i < NUM_CLIENTS; ++i) {
+        delete[] all_requests[i];
+    }
+
+    release(queue);
+    return 0;
 }
